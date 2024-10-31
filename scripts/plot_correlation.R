ab3da37c446fb6366f66565a94f3128038ae3392
Kevin Boyd <63878894+kevinboyd76@users.noreply.github.com>
2024-10-31 15:07:07 -0500

    Update plot_correlation.R

--- scripts/plot_correlation.R
+++ scripts/plot_correlation.R
@@ -3,6 +3,7 @@
 # Load required libraries
 library(dplyr)
 library(corrplot)
+library(Cairo)  # Add Cairo library
 
 # Capture command-line arguments
 args <- commandArgs(trailingOnly = TRUE)
@@ -40,7 +41,7 @@ M <- cor(fragCount %>% select(-chrom, -bin) %>% log2(), use = "complete.obs")
 
 # Generate correlation plot
 output_file <- file.path(output_dir, "fragCount_correlation_plot.png")
-png(output_file, width = 800, height = 800)
+Cairo::CairoPNG(filename = output_file, width = 800, height = 800)
 corrplot(M, method = "color", outline = TRUE, addgrid.col = "darkgray", order = "hclust",
          addrect = 3, rect.col = "black", rect.lwd = 3, cl.pos = "b", tl.col = "indianred4",
          tl.cex = 1, cl.cex = 1, addCoef.col = "black", number.digits = 2, number.cex = 1,
