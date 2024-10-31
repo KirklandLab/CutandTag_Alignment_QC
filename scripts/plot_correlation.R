5d6b76b3c1de457df9d8c46d385f72c92e88244d
Kevin Boyd <63878894+kevinboyd76@users.noreply.github.com>
2024-10-31 16:40:46 -0500

    Update plot_correlation.R

--- scripts/plot_correlation.R
+++ scripts/plot_correlation.R
@@ -38,11 +38,15 @@ for (file_path in input_files) {
 # Calculate correlation matrix
 M <- cor(fragCount %>% select(-chrom, -bin) %>% log2(), use = "complete.obs")
 
+# Dynamically set addrect based on the number of samples
+num_samples <- ncol(M)
+addrect_value <- ifelse(num_samples >= 3, 3, num_samples)
+
 # Generate correlation plot
 output_file <- file.path(output_dir, "fragCount_correlation_plot.pdf")
 pdf(output_file, width = 8, height = 8)
 corrplot(M, method = "color", outline = TRUE, addgrid.col = "darkgray", order = "hclust",
-         addrect = 3, rect.col = "black", rect.lwd = 3, cl.pos = "b", tl.col = "indianred4",
+         addrect = addrect_value, rect.col = "black", rect.lwd = 3, cl.pos = "b", tl.col = "indianred4",
          tl.cex = 1, cl.cex = 1, addCoef.col = "black", number.digits = 2, number.cex = 1,
          col = colorRampPalette(c("midnightblue", "white", "darkred"))(100))
 dev.off()
