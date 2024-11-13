b480ba5027445c65009242674ed3a5303e79701d
Kevin Boyd <63878894+kevinboyd76@users.noreply.github.com>
2024-11-13 17:12:43 -0600

    Update plot_peakSummary.R

--- scripts/plot_peakSummary.R
+++ scripts/plot_peakSummary.R
@@ -6,13 +6,11 @@ library(ggplot2)
 library(viridis)
 library(ggpubr)
 library(GenomicRanges)
-library(chromVAR)
 
 # Capture command-line arguments
 args <- commandArgs(trailingOnly = TRUE)
-input_files <- args[-c(length(args) - 1, length(args))]  # All but last two arguments are input files
-output_dir <- args[length(args) - 1]                    # Second-to-last argument is the output directory
-bam_dir <- args[length(args)]                           # Last argument is the BAM directory
+input_files <- args[-length(args)]  # All but last argument are input files
+output_dir <- args[length(args)]    # Last argument is the output directory
 
 # Ensure the output directory exists
 if (!dir.exists(output_dir)) {
@@ -74,73 +72,7 @@ fig2 <- ggplot(peakData, aes(x = Histone, y = width, fill = Histone)) +
   ylab("Width of Peaks") +
   xlab("")
 
-# Plot 3: % of Peaks Reproduced (overlap between replicates)
-reproducibility_data <- data.frame()
-for (hist in histList) {
-  rep1 <- paste0(hist, "_rep1")
-  rep2 <- paste0(hist, "_rep2")
-  if (rep1 %in% names(peak_granges_list) && rep2 %in% names(peak_granges_list)) {
-    # Find overlapping peaks between replicates
-    overlaps <- findOverlaps(peak_granges_list[[rep1]], peak_granges_list[[rep2]])
-    reproducible_count <- length(unique(queryHits(overlaps)))
-    total_peaks <- length(peak_granges_list[[rep1]]) + length(peak_granges_list[[rep2]])
-    reproducibility_rate <- (reproducible_count / (total_peaks / 2)) * 100
-    reproducibility_data <- rbind(reproducibility_data, data.frame(Histone = hist, ReproducibilityRate = reproducibility_rate))
-  }
-}
-
-fig3 <- ggplot(reproducibility_data, aes(x = Histone, y = ReproducibilityRate, fill = Histone)) +
-  geom_bar(stat = "identity") +
-  geom_text(aes(label = round(ReproducibilityRate, 2)), vjust = -0.5) +
-  theme_bw(base_size = 18) +
-  ylab("% of Peaks Reproduced") +
-  xlab("")
-
-# Plot 4: Fraction of Reads in Peaks (FRiP) using chromVAR
-inPeakData <- data.frame()
-
-for (hist in histList) {
-  for (rep in unique(peakData$Replicate[peakData$Histone == hist])) {
-    # Define the peak regions for the current histone and replicate
-    peakInfo <- peakData %>%
-      filter(Histone == hist & Replicate == rep)
-    peak_gr <- GRanges(seqnames = peakInfo$chrom, 
-                       ranges = IRanges(start = peakInfo$start, end = peakInfo$end),
-                       strand = "*")
-    
-    # Path to the BAM file for the current histone and replicate
-    bamFile <- file.path(bam_dir, paste0(hist, "_", rep, "_bowtie2.mapped.bam"))
-    print(paste("Reading in file:", bamFile))
-    
-    # Use chromVAR to get fragment counts within peaks and total fragments
-    fragment_counts <- getCounts(bamFile, peak_gr, paired = TRUE, by_rg = FALSE, format = "bam")
-    
-    # Check if fragment_counts has valid data
-    if (length(fragment_counts$total) > 0) {
-      # Total fragments (MappedFragNum) and fragments in peaks (inPeakN)
-      inPeakN <- sum(counts(fragment_counts)[,1])  # Fragments in peaks
-      MappedFragNum <- fragment_counts$total  # Total mapped fragments in BAM
-      
-      # Store results in a data frame
-      inPeakData <- rbind(inPeakData, data.frame(Histone = hist, Replicate = rep, inPeakN = inPeakN, MappedFragNum = MappedFragNum))
-    } else {
-      print(paste("Warning: No data found in fragment counts for", hist, rep))
-    }
-  }
-}
-
-# Calculate FRiP scores
-frip <- inPeakData %>% 
-  mutate(FRiP = (inPeakN / MappedFragNum) * 100)
-
-fig4 <- ggplot(frip, aes(x = Histone, y = FRiP, fill = Histone)) +
-  geom_boxplot() +
-  geom_jitter(aes(color = Replicate), position = position_jitter(0.15)) +
-  theme_bw(base_size = 18) +
-  ylab("% of Fragments in Peaks (FRiP)") +
-  xlab("")
-
 # Arrange and save all plots
-final_plot <- ggarrange(fig1, fig2, fig3, fig4, ncol = 2, nrow = 2, common.legend = TRUE, legend = "bottom")
+final_plot <- ggarrange(fig1, fig2, ncol = 2, nrow = 1, common.legend = TRUE, legend = "bottom")
 output_file <- file.path(output_dir, "peak_summary_plot.png")
 ggsave(output_file, final_plot, width = 14, height = 10)
